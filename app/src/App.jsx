6ea2818cdf433d5946b34f38ab4035bc34d7633d
josedrios <joserios0531@gmail.com>
2025-06-13 08:36:47 -0700

    Setting up dev environment before starting project

---
+++ app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './styles/main.scss'
+
+function App() {
+  return (
+    <>
+      This is my personal website... v2 <span>(test)</span>
+    </>
+  )
+}
+
+export default App
