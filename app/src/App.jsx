0724b206ad4d354c25b3589086ba18186e8b7dba
josedrios <joserios0531@gmail.com>
2025-06-17 13:48:41 -0700

    Getting started on the typography foundation of the website, started on design system

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,13 +1,24 @@
 import './styles/main.scss';
+import Header from './components/Header';
 
 function App() {
   return (
     <div id="website-container">
+      <Header />
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos consectetur iure deleniti
-        amet culpa ipsum dolorum! Reiciendis corrupti libero quis voluptate, sapiente, fugiat sunt
-        harum labore obcaecati neque vitae assumenda!
+        This is an example sentence to show how a p tag would look. Im going to write a little more gibberish just to show its text wrapping capabilities and how it would look like in the UI.
       </p>
+      <p>This is a <span>span</span> word in a sentence</p>
+      <p>This is a <strong>bold</strong> word in a sentence</p>
+      <p>This is a <i>italic</i> word in a sentence</p>
+      <h1>Spacing Increments</h1>
+      <div className='spacing-xs'/>
+      <div className='spacing-sm'/>
+      <div className='spacing-md'/>
+      <div className='spacing-lg'/>
+      <div className='spacing-xl'/>
     </div>
   );
 }
---
+++ app/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="left-header">
+                <h1>josedrios<span>.</span></h1>
+            </div>
+            <nav>
+                
+            </nav>
+        </header>
+    )
+}
