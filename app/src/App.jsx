49fd4162a67b25a5b855beda115b19e477f84bea
josedrios <joserios0531@gmail.com>
2025-06-16 22:11:11 -0700

    Setting up basic scss for base, reset and variables (rudementary variables, layout for the foundation of the website and the graph paper grid background

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './styles/main.scss'
+import { useState } from 'react';
+import './styles/main.scss';
 
 function App() {
   return (
-    <>
-      This is my personal website... v2 <span>(test)</span>
-    </>
-  )
+    <div id="website-container">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos consectetur iure deleniti
+        amet culpa ipsum dolorum! Reiciendis corrupti libero quis voluptate, sapiente, fugiat sunt
+        harum labore obcaecati neque vitae assumenda!
+      </p>
+    </div>
+  );
 }
 
-export default App
+export default App;
