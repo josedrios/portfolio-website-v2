751794702e55f9c4d68b7f4c2a34715461788397
josedrios <joserios0531@gmail.com>
2025-07-01 15:26:16 -0700

    Fixed /drafts gitignore, styled blog list pagination navigation section

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { getMedianDate } from '@/util/dateUtil';
+import { LeftArrowIcon, RightArrowIcon } from './Icons';
 
 export default function BlogList({ posts, currentPage, totalPages }) {
     return (
@@ -9,7 +10,9 @@ export default function BlogList({ posts, currentPage, totalPages }) {
                 <div key={post.slug}>
                     <Link href={`/blog/${post.slug}`} className="blog-link">
                         <div className="card">
-                            <h2><span>{post.title}</span></h2>
+                            <h2>
+                                <span>{post.title}</span>
+                            </h2>
                             <p className="blog-card-info">{post.info}</p>
                             <p className="blog-card-date">
                                 {post.date ? getMedianDate(post.date) : 'No date'}
@@ -19,19 +22,29 @@ export default function BlogList({ posts, currentPage, totalPages }) {
                 </div>
             ))}
 
-            <div className="pagination">
+            <p className="pagination">
                 {currentPage > 1 && (
-                    <Link href={currentPage === 2 ? '/blog' : `/blog/pages/${currentPage - 1}`}>
-                        Prev
+                    <Link
+                        href={
+                            currentPage === 2 ? '/blog' : `/blog/pages/${currentPage - 1}`
+                        }
+                        className="blog-list-nav-button"
+                    >
+                        <LeftArrowIcon />
                     </Link>
                 )}
-                <span>Page {currentPage} of {totalPages}</span>
+                <span>
+                    Page {currentPage} of {totalPages}
+                </span>
                 {currentPage < totalPages && (
-                    <Link href={`/blog/pages/${currentPage + 1}`}>
-                        Next
+                    <Link
+                        href={`/blog/pages/${currentPage + 1}`}
+                        className="blog-list-nav-button"
+                    >
+                        <RightArrowIcon />
                     </Link>
                 )}
-            </div>
+            </p>
         </div>
     );
 }
